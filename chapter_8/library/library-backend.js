37b482ebfada80bff45aa1f610b78d1f9e13b765
Jessica Sarlin <jessica.sarlin@gmail.com>
2022-02-21 12:18:21 +0200

    8.1 done

--- chapter_8/library/library-backend.js
+++ chapter_8/library/library-backend.js
@@ -89,12 +89,31 @@ let books = [
 ]
 
 const typeDefs = gql`
+  type Book {
+    title: String!
+    published: Int!
+    author: Author!
+    genres: [String]! 
+    id: ID!
+  }
+
+  type Author {
+    name: String!
+    born: Int
+    books: [Book]! 
+    id: ID!
+  }
+
   type Query {
+    bookCount: Int!
+    authorCount: Int!
   }
 `
 
 const resolvers = {
   Query: {
+    bookCount: () => books.length,
+    authorCount: () => authors.length
   }
 }
 
