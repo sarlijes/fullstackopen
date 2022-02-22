96ffd7f3f58795ca1f15e06dbf03c5a0e5296d4d
Jessica Sarlin <jessica.sarlin@gmail.com>
2022-02-22 12:46:29 +0200

    8.17 done

--- chapter_8/library/frontend/src/components/Books.js
+++ chapter_8/library/frontend/src/components/Books.js
@@ -4,7 +4,9 @@ const ALL_BOOKS = gql`
   query {
     allBooks {
       title
-      author
+      author {
+        name
+      }
       published
     }
   }
@@ -39,7 +41,7 @@ const Books = (props) => {
           {books.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
-              <td>{a.author}</td>
+              <td>{a.author.name}</td>
               <td>{a.published}</td>
             </tr>
           ))}
