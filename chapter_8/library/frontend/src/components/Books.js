93b13146fd2928cc884e0f2f84d8a0ae51d8ab25
Jessica Sarlin <jessica.sarlin@gmail.com>
2022-02-22 14:16:40 +0200

    8.19 WIP - UI buttons for genres

--- chapter_8/library/frontend/src/components/Books.js
+++ chapter_8/library/frontend/src/components/Books.js
@@ -1,6 +1,7 @@
+import { useState } from 'react'
 import { gql, useQuery } from '@apollo/client'
 
-export const ALL_BOOKS = gql`
+export const ALL_BOOKS_OLD = gql`
   query {
     allBooks {
       title
@@ -8,11 +9,25 @@ export const ALL_BOOKS = gql`
         name
       }
       published
+      genres
     }
   }
 `
-const Books = (props) => {
+export const ALL_BOOKS = gql`
+  query findBooksByGenre($genreToSearch: String) {
+    allBooks(genre: $genreToSearch) {
+      title
+      author {
+        name
+      }
+      published
+      genres
+    }
+  }
+`
 
+const Books = (props) => {
+  const [selectedGenre, setSelectedGenre] = useState(null)
   const result = useQuery(ALL_BOOKS)
 
   if (!props.show) {
@@ -22,6 +37,11 @@ const Books = (props) => {
     return <div>loading...</div>
   }
   const books = result.data.allBooks
+
+  let genreOptions = new Set();
+
+  books.forEach((a) => a.genres.forEach(genreOptions.add, genreOptions));
+  genreOptions = Array.from(genreOptions)
 
   return (
     <div>
@@ -43,6 +63,13 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+      <h2>filter by genre</h2>
+      <div>
+        {genreOptions.map((genre) => (
+          <button key={genre} onClick={() => setSelectedGenre(genre)}>{genre}</button>
+        ))}
+        <button key="all" onClick={() => setSelectedGenre(null)}>All genres</button>
+      </div>
     </div>
   )
 }
