43dc7a8493741e08ae4ee2932fa1a894f4e1bec2
Jessica Sarlin <jessica.sarlin@gmail.com>
2022-02-24 14:50:20 +0200

    Install express + add files

---
+++ chapter_9/ts_project/file.ts
@@ -0,0 +1,18 @@
+
+type Operation = 'multiply' | 'add' | 'divide';
+
+const calculator = (a: number, b: number, op: Operation): number => {
+  if (op === 'multiply') {
+    return a * b;
+  } else if (op === 'add') {
+    return a + b;
+  } else if (op === 'divide') {
+    if (b === 0) throw new Error('can\'t divide by 0!');
+    return a / b;
+  }
+  return -1;
+}
+
+const multiplicator = (a: number, b: number, printText: string) => {
+  console.log(printText, a * b);
+}
