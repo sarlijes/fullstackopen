2c8bc9842a5d35ca088d16f6b7c149a8ad48c5f6
Jessica Sarlin <jessica.sarlin@gmail.com>
2022-02-25 15:12:49 +0200

    Add code fron exercise

--- chapter_9/half_stack/my-app/src/App.tsx
+++ chapter_9/half_stack/my-app/src/App.tsx
@@ -37,20 +37,58 @@ const Total = (props: TotalProps) => {
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  // new types
+  interface CoursePartBase {
+    name: string;
+    exerciseCount: number;
+    type: string;
+  }
+
+  interface CourseNormalPart extends CoursePartBase {
+    type: "normal";
+    description: string;
+  }
+  interface CourseProjectPart extends CoursePartBase {
+    type: "groupProject";
+    groupProjectCount: number;
+  }
+
+  interface CourseSubmissionPart extends CoursePartBase {
+    type: "submission";
+    description: string;
+    exerciseSubmissionLink: string;
+  }
+
+  type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart;
+
+  // this is the new coursePart variable
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is the leisured course part",
+      type: "normal"
+    },
+    {
+      name: "Advanced",
+      exerciseCount: 7,
+      description: "This is the harded course part",
+      type: "normal"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      type: "groupProject"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+      type: "submission"
     }
-  ];
+  ]
 
   return (
     <div>
