bf561fdd3ca3cb561162e20c2ba4ce86a537a22d
Jessica Sarlin <jessica.sarlin@gmail.com>
2022-02-28 14:42:40 +0200

    9.19 done + rename variables + copy-paste types to frontent

--- chapter_9/patientor/backend/src/services/diagnoseService.ts
+++ chapter_9/patientor/backend/src/services/diagnoseService.ts
@@ -1,9 +1,9 @@
 import diagnoseData from '../../data/diagnoses.json';
-import { Diagnose } from '../types';
+import { Diagnosis } from '../types';
 
-const diagnoses: Array<Diagnose> = diagnoseData;
+const diagnoses: Array<Diagnosis> = diagnoseData;
 
-const list = (): Array<Diagnose> => {
+const list = (): Array<Diagnosis> => {
   return diagnoses;
 };
 
--- chapter_9/patientor/frontend/src/types.ts
+++ chapter_9/patientor/frontend/src/types.ts
@@ -1,3 +1,12 @@
+export interface HospitalDischarge {
+  date: string;
+  criteria: string;
+}
+export interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
 export interface Diagnosis {
   code: string;
   name: string;
@@ -5,16 +14,62 @@ export interface Diagnosis {
 }
 
 export enum Gender {
-  Male = "male",
-  Female = "female",
-  Other = "other"
+  Female = 'female',
+  Male = 'male',
+  Other = 'other',
 }
 
 export interface Patient {
   id: string;
   name: string;
-  occupation: string;
+  dateOfBirth: string;
+  ssn: string;
   gender: Gender;
-  ssn?: string;
-  dateOfBirth?: string;
+  occupation: string;
+  entries: Entry[];
 }
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: HospitalDischarge
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave
+}
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export type PublicPatient = Omit<Patient, 'ssn' | 'entries'>;
+
+export type NewPatient = Omit<Patient, 'id'>;
+
+// Define special omit for unions
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+// Define Entry without the 'id' property
+export type EntryWithoutId = UnionOmit<Entry, 'id'>;
